399c7866d9ddca3eaa11ae87acad7209f91b5a08
gokhanagkurt <goekhan.agkurt@outlook.de>
2023-05-31 15:42:25 +0200

    change number again

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         int finalResult = resultOfAddition(10, 20);
-        String finalText = resultText(25, 55);
+        String finalText = resultText(205, 505);
         System.out.println(finalResult);
         System.out.println(finalText);
     }
