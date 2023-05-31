f5d8fa8708db58ec0c615654738ef9620048564a
gokhanagkurt <goekhan.agkurt@outlook.de>
2023-05-31 14:45:04 +0200

    first Groupwork

---
+++ src/main/java/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        int finalResult = resultOfAddition(10, 20);
+        String finalText = resultText(22, 45);
+        System.out.println(finalResult);
+        System.out.println(finalText);
+    }
+
+
+
+    public static int resultOfAddition(int firstValue, int secondValue) {
+        int result = firstValue + secondValue;
+        return result;
+    }
+
+
+
+    public static String resultText(int firstValue, int secondValue) {
+        int result = firstValue + secondValue;
+        return "Summe: " + result;
+    }
+}
